8943bbbcdbc039005844f7d62dee30e0e1cad229
Tom-Berends-EWE <tom.berends@ewe-netz.de>
2024-02-28 13:45:13 +0100

    added retrieve command structure

---
+++ retrieve.py
@@ -0,0 +1,9 @@
+__all__ = ['retrieve']
+
+
+def retrieve(docs_dirs: tuple[str],
+             embeddings_model: str,
+             num_docs: int,
+             overwrite_cached_embeddings: bool,
+             run_psql_instance: bool) -> None:
+    pass
